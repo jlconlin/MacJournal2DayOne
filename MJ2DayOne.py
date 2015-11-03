7640cfe920f9bd705b8cc65872b2d9296d6094db
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2015-11-03 10:25:59 -0500

    Updating a number of things.
    I.e., I didn't commit often enough.

--- MJ2DayOne.py
+++ MJ2DayOne.py
@@ -1,8 +1,12 @@
 
 from __future__ import print_function
 
+import os
+import subprocess
 import sys
 sys.path.append("parser")
+
+import pypandoc
 
 import MJParser
 
@@ -21,7 +25,7 @@ dayOneBasicString = """
 		<string>MacJournal/{agentVersion}</string>
 	</dict>
 	<key>Entry Text</key>
-	<string>This is a test. I want to know what happens if I do this here.</string>
+	<string>{entryText}</string>
 	<key>Location</key>
 	<dict>
 		<key>Latitude</key>
@@ -41,12 +45,15 @@ dayOneBasicString = """
 </plist>
 """
 
-def entryData(Entry):
+def entryData(Entry, mjDoc):
     """
     entryData will return a dictionary that contains the meta data used to
     create a Day One entry. The meta data comes from the MacJournal entry: Entry.
     """
     metaData = {}
+
+    generator =  mjDoc.macjournalml.getElementsByTagName('generator')[0]
+    metaData['agentVersion'] = generator.getAttribute('version')
 
     # Date
     fmt = "%Y-%m-%dT%H:%M:%S.%f"
@@ -62,7 +69,34 @@ def entryData(Entry):
     tags = [" "*8 + "<string>{}</string>\n".format(word) for word in keywords]
     metaData['tags'] = tags
 
+    # Entry text
+    filename = os.path.join(mjDoc.Content, Entry.filename)
+    filetext = subprocess.check_output(
+        'ruby rtf2markdown.rb "{}"'.format(filename), shell=True)
+    metaData['text'] = filetext
+
     return metaData
+
+def entryText(Entry, mjDoc, format="txt"):
+    """
+    entryText will extract the text for the entry. It returns the text as a
+    string.
+
+    format: The format to which you want the entry text formatted.
+    """
+    filename = os.path.join(mjDoc.Content, Entry.filename)
+
+    # Copy file to cwd. I don't know why Python can't otherwise find the file.
+    os.system( 'cp "{}" .'.format(filename) )
+
+    # Convert the file to preferred format
+    cmd = 'textutil -convert {} "{}" -stdout'.format(format, Entry.filename)
+    content = subprocess.check_output(cmd, shell=True)
+
+    # Delete copied file
+    os.system( 'rm "{}"'.format(Entry.filename) )
+
+    return content
 
 if __name__ == "__main__":
     print("\nI'm converting from MacJournal to DayOne.\n")
@@ -74,16 +108,26 @@ if __name__ == "__main__":
         help='MacJournal document location.')
     parser.add_argument('--xml', type=str, default=None,
         help='Write XML file for human readability')
+    parser.add_argument('--format', type=str, default="txt",
+        help='Format to which the entry text will be converted')
+
+    parser.add_argument('--journal-name', type=str, default="Journal",
+        help="Name of DayOne journal file; '.dayone' will be appended.")
 
     args = parser.parse_args()
 
-    mjDoc = MJParser.mjdoc(args.mjdoc[0], verbose=True)
+#   mjDoc = MJParser.mjdoc(args.mjdoc[0], verbose=True)
 
     if args.xml:
         mjDoc.MakeXMLFile(args.xml)
 
-    # Find some global data for string formatting
-    mjArgs = {}
-    generator =  mjDoc.macjournalml.getElementsByTagName('generator')[0]
-    mjArgs['agentVersion'] = generator.getAttribute('version')
+    mjEntry = mjDoc.Journals['Daily Journal'].Journals['2015'].Entries[0]
+    metaData = entryData(mjEntry, mjDoc)
+    metaData['entryText'] = entryText(mjEntry, mjDoc, format=args.format)
+
+    journalPath = "{}.dayone".format(args.journal_name)
+    if not os.path.exists(journalPath):
+        os.makedirs( os.path.join(journalPath, "entries") )
+        os.makedirs( os.path.join(journalPath, "photos") )
+
 
